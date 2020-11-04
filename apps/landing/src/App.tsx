af970dc74778215cfac197145c58530963d39ebe
Ezio Santori <Ezio.Santori@wolterskluwer.com>
2020-11-04 08:51:19 +0100

    rush workspace test

---
+++ apps/landing/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Thing } from "@shareds/components"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Thing />
+    </div>
+  );
+}
+
+export default App;
